4266ed668301d9dfdecbde85c352d5c00320868f
teenhokage15 <teenhokage15@gmail.com>
2026-01-03 22:33:23 +0530

    fix google 0Auth using env-based refresh token

--- server/utils/googleAuth.js
+++ server/utils/googleAuth.js
@@ -1,17 +1,18 @@
 const { google } = require("googleapis");
 
 const getOAuthClient = () => {
+  const credentials = JSON.parse(process.env.GOOGLE_OAUTH_CREDENTIALS);
+  const { client_id, client_secret } = credentials.installed;
+
   const oAuth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URI
+    client_id,
+    client_secret,
+    "https://patent-filing-portal.onrender.com/oauth2callback"
   );
 
-  if (process.env.GOOGLE_REFRESH_TOKEN) {
-    oAuth2Client.setCredentials({
-      refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-    });
-  }
+  oAuth2Client.setCredentials({
+    refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+  });
 
   return oAuth2Client;
 };
